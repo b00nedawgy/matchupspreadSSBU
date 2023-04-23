0632c9fa5abad6f29ec3774e39b1d9347b8f7e73
Brendan Boone <brboone@ucsc.edu>
2023-04-22 23:01:53 -0700

    adding charsl/ and fixed previous list issue

--- scripts/setup.js
+++ scripts/setup.js
@@ -2,21 +2,26 @@ const list_of_characters = ['Mario', 'Donkey-Kong', 'Link', 'Samus', 'Dark-Samus
 
 var chars = [];
 
-function btnfunc(name) {
-    chars.push(name);
-    console.log(name);
-    console.log(chars);
-}
-
 function printBtn() {
+    //character buttons
     for (var i = 0; i < list_of_characters.length; i++) {
+        let name = list_of_characters[i];
         var btn = document.createElement("button");
-        var img = 'charsl/' + list_of_characters[i] + '.png';
-        btn.setAttribute("id", list_of_characters[i]);
+        var img = 'charsl/' + name + '.png';
+        btn.setAttribute("id", name);
         btn.style.backgroundImage = 'url(' + img + ')';
-        btn.addEventListener('click', function(){
-            btnfunc(list_of_characters[i]);
+        btn.addEventListener('click', function() {
+            chars.push(name);
+            console.log(name);
+            console.log(chars);
         });
         document.getElementById("player_characters").appendChild(btn);
     }
+
+    //reset button
+    var rstbtn = document.getElementById("reset-button");
+    rstbtn.addEventListener('click', function() {
+        chars = [];
+        console.log(chars);
+    });
 }
