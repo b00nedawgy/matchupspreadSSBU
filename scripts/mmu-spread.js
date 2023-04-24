df8fca5cc888b14fd340b685acab6c190bf548dd
Brendan Boone <brboone@ucsc.edu>
2023-04-24 10:10:46 -0700

    removing some tests in mmu

--- scripts/mmu-spread.js
+++ scripts/mmu-spread.js
@@ -1,7 +1,6 @@
 const mlist_of_characters = ['Mario', 'Donkey-Kong', 'Link', 'Samus', 'Dark-Samus', 'Yoshi', 'Kirby', 'Fox', 'Pikachu', 'Luigi', 'Ness', 'Captain-Falcon', 'Jigglypuff', 'Peach', 'Daisy', 'Bowser', 'Ice-Climbers', 'Sheik', 'Zelda', 'Dr-Mario', 'Pichu', 'Falco', 'Marth', 'Lucina', 'Young-Link', 'Ganondorf', 'Mewtwo', 'Roy', 'Chrom', 'Mr-Game-Watch', 'Meta-Knight', 'Pit', 'Dark-Pit', 'Zero-Suit-Samus', 'Wario', 'Snake', 'Ike', 'Pokemon-Trainer', 'Diddy-Kong', 'Lucas', 'Sonic', 'King-Dedede', 'Olimar', 'Lucario', 'R.O.B.', 'Toon-Link', 'Wolf', 'Villager', 'Mega-Man', 'Wii-Fit-Trainer', 'Rosalina-Luma', 'Little-Mac', 'Greninja', 'Mii-Brawler', 'Mii-Swordfighter', 'Mii-Gunner', 'Palutena', 'Pac-man', 'Robin', 'Shulk', 'Bowser-Jr', 'Duck-Hunt', 'Ryu', 'Ken', 'Cloud', 'Corrin', 'Bayonetta', 'Inkling', 'Ridley', 'Simon', 'Richter', 'King-K-Rool', 'Isabelle', 'Incineroar', 'Piranha-Plant', 'Joker', 'Hero', 'Banjo-Kazooie', 'Terry', 'Byleth', 'Min-Min', 'Steve', 'Sephiroth', 'Pyra-Mythra', 'Kazuya', 'Sora'];
 
 function printImgs() {
-    console.log("printImgs() running");
     //characters
     for (var i = 0; i < mlist_of_characters.length; i++) {
         let name = mlist_of_characters[i];
@@ -11,5 +10,4 @@ function printImgs() {
         btn.src = img;
         document.getElementById("mmu_characters").appendChild(btn);
     }
-    console.log("printImgs() ran");
 }
